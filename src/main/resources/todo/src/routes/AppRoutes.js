42dc9e8dc9ee3c270c155bfabbd380996273a4ba
KangSungLee <rkdtjd1020@naver.com>
2025-02-16 17:45:21 +0900

    project

--- src/main/resources/todo/src/routes/AppRoutes.js
+++ src/main/resources/todo/src/routes/AppRoutes.js
@@ -1,18 +1,17 @@
-import React, { useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage'; 
 import SignupPage from '../pages/SignupPage';
 import ToDoPage from '../pages/ToDoPage';
 
 const AppRoutes = () => {
-  const token = localStorage.getItem('token');
 
   return (
     <Routes>
-      <Route path="/" element={!token ? <ToDoPage /> : <LoginPage />} />
-      <Route path="/login" element={token ? <ToDoPage /> : <LoginPage />} />
+      <Route path="/" element={<ToDoPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
-      <Route path="/home" element={token ? <ToDoPage /> : <LoginPage />} />
+      <Route path="/home" element={<ToDoPage />} />
     </Routes>
   );
 };
